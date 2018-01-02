adbb5b36246dc74ed1450689af801db840e2774e
Dilawar Singh <dilawars@ncbs.res.in>
2018-01-02 14:53:43 +0530

    Without LATEX.
    O1 sets camera ttl pulse up and 2 brings it down. Works only before 's' is pressed.
    
    Craete backup while copyong.
    
    Added legal commands.
    
    integrated command 1 and 2.
    1-> start camera
    2->stop camera.
    
    ready to be launched.

---
+++ PointGreyCamera/config.h
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  config.h.in
+ *
+ *    Description:  Configuration variables in one place. This file  is also
+ *    parsed by python script to its essential and this is not removed or
+ *    changed without consulting the python script first.
+ *
+ *        Version:  1.0
+ *        Created:  Tuesday 06 December 2016 02:03:48  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Dilawar Singh (), dilawars@ncbs.res.in
+ *   Organization:  NCBS Bangalore
+ *
+ * =====================================================================================
+ */
+
+#ifndef  config_INC
+#define  config_INC
+
+#define SOCK_PATH  "/tmp/eye_blink_socket"
+
+/* Block to write. */
+#define BLOCK_SIZE  4096 
+
+#define FRAME_HEIGHT            512
+#define FRAME_WIDTH             640
+#define EXPOSURE_TIME_IN_US     2000
+#define EXPECTED_FPS            200
+
+#endif   /* ----- #ifndef config_INC  ----- */
