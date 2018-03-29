17a2c28737a3ba45439a66836cab22e7ec59eba2
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-29 11:53:53 +0530

    use command 1 and 2.

--- src/config.h
+++ src/config.h
@@ -2,8 +2,8 @@
 #define CONFIG.H_H
 
 #define         SESSION_TYPE        'S'
-#define         SESSION_NUM         1
-#define         ANIMAL_NAME         "k3"
+#define         SESSION_NUM         02
+#define         ANIMAL_NAME         "751"
 
 
 #endif /* end of include guard: CONFIG.H_H */
