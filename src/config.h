394de8c693f19cc0f1b715b7d5934b9d5decbf02
Anzal KS <anzal.ks@gmail.com>
2018-12-08 15:56:28 +0530

    Integrated rotary encoder.

--- src/config.h
+++ src/config.h
@@ -1,9 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define         SESSION_TYPE        'S'
+#define         SESSION_TYPE        '2'
 #define         SESSION_NUM         1
-#define         ANIMAL_NAME         "k3"
+#define         ANIMAL_NAME         "k2"
 
 
 #endif /* end of include guard: CONFIG_H */
