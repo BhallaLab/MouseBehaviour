ff5aa9ea1118172add41c536170e48ef555c49dd
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-07-16 19:24:54 +0530

    updated docs.

--- docker/setup_venv.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-add-apt-repository -y -u ppa:deadsnakes/ppa
-apt-get install -y python3.6-venv python3.6-tk
-python3.6 -m venv ~/PY36
-source ~/PY36/bin/activate
-pip install -r ../requirements.txt
