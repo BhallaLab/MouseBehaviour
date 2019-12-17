ad2e7e9833ad6eec458e2a29d664f67c28a6f151
Dilawar Singh <dilawar.s.rajput@gmail.com>
2019-12-17 18:02:28 +0530

    Use tifffile instead of libtiff. Python module is way behind libtiff.

--- analysis/find_latency.py
+++ analysis/find_latency.py
@@ -7,7 +7,11 @@ import matplotlib as mpl
 mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy.optimize as sco
 pd.plotting.register_matplotlib_converters()
+
+def fit_linear(x, m, c):
+    return m*x+c
 
 def main():
     infile = sys.argv[1]
@@ -24,6 +28,7 @@ def main():
     t0 = t0.astype(np.float)/1e6    # ms
     t1 = t1.astype(np.float)/1e6    # ms
 
+
     plt.figure(figsize=(12,3))
     ax1 = plt.subplot(131)
     ax2 = plt.subplot(132)
@@ -38,12 +43,19 @@ def main():
     ax2.plot(t0, t0 - t1, label='tCam-tArd')
     #  ax2.plot(t0, t0 - t1, label='tCam-tArdReceived')
     ax2.plot(t0, t1 - t2, label='tArdReceived-tArd')
+
+    # fit
+    y = t1-t2
+    (m, c), _ = sco.curve_fit(fit_linear, t0, y)
+    tJitter = y - fit_linear(t0, m, c)
+    ax2.plot(t0, fit_linear(t0, m, c), label=f'Fit $y={m:g}x+{c:g}$')
+
     ax2.set_xlabel('Time (ms)')
     ax2.set_ylabel('Time (ms)')
     ax2.legend()
 
     ax3.hist(t0-t1, alpha=0.5, density=True, label='tCam-tArdReceived')
-    ax3.hist(t1-t2, alpha=0.5, density=True, label='tArdReceived-tArd')
+    ax3.hist(tJitter, alpha=0.5, density=True, label='tArdReceived-tArd')
     ax3.legend()
     ax3.set_xlabel('Time (ms)')
 
@@ -54,6 +66,7 @@ def main():
     ref['tcam'] = t0
     ref['tArdReceived'] = t1
     ref['tArd'] = t2
+    ref['tDiffJitter'] = tJitter
     ref.to_csv(f'{outfilePrefix}.csv', index=False)
 
 
